4d3a9bf0408f23da163ca2e84b76fc8cc4f07d0e
DaniaHG <dhernandezg14@miumg.edu.gt>
2021-10-25 22:06:58 -0600

    commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
+import { PrincipalComponent } from './principal/principal.component';
+import { MostrarTablaComponent } from './componentes/vuelo/mostrar-tabla/mostrar-tabla.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', redirectTo:'/inicio',pathMatch:'full'},
+  {path:'inicio', component: PrincipalComponent},
+  {path:'vuelo', component: MostrarTablaComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/componentes/vuelo/mostrar-tabla/mostrar-tabla.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mostrar-tabla',
+  templateUrl: './mostrar-tabla.component.html',
+  styleUrls: ['./mostrar-tabla.component.css']
+})
+export class MostrarTablaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
