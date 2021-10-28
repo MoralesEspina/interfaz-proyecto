2c5b31de31bec12d9f1d6c7af8d5eabec6d97c97
Dylan Alexis Tobar Orellana <dylantobar2001@gmail.com.com>
2021-10-27 23:15:40 -0600

    agregando seleccion de origen y destino

--- src/app/principal/principal.component.ts
+++ src/app/principal/principal.component.ts
@@ -1,8 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { TablaDataSource, Ruta } from './tabla-datasource';
 import { Ruta1Service } from 'src/app/principal/services/principal.service';
-
-
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @Component({
 
@@ -11,21 +9,30 @@ import { Ruta1Service } from 'src/app/principal/services/principal.service';
   styleUrls: ['./principal.component.css']
 })
 export class PrincipalComponent implements OnInit {
-  dataSource: TablaDataSource;
-
-  displayedColumns = ['iataCode', 'pais', 'ciudad'];
 
 
-  constructor(private RegistroRuta1: Ruta1Service){
-    this.dataSource = new TablaDataSource();}
-
-  ngOnInit()  {
-    this.RegistroRuta1.getRuta().subscribe(
-      res=>{
-        this.dataSource.data= res;
-      }
-    )
+  constructor(private http: HttpClient) {
 
   }
+  selected1: string;
+  selected: string;
+  collection = [{'iataCode':this.getCollection,'pais':this.getCollection, 'ciudad':this.getCollection}];
+
+  ngOnInit() {
+    this.getCollection();
+
+  }
+
+  getCollection() {
+
+    this.http
+      .get<any>('https://aeropuerto-dw.herokuapp.com/aeropuertos').subscribe((res: any) => {
+        this.collection = res;
+      }, error => {
+        console.log({ error });
+      })
+  }
+
+
 
 }
--- src/app/principal/services/principal.service.ts
+++ src/app/principal/services/principal.service.ts
@@ -1,11 +1,12 @@
 import{HttpClient, HttpClientModule}from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/internal/Observable';
-import {  TablaDataSource, Ruta } from '../tabla-datasource';
+import { Ruta } from '../tabla-datasource';
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class Ruta1Service {
  URL = "https://aeropuerto-dw.herokuapp.com";
   constructor(private http : HttpClient) { }
