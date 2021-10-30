c66e0add31a14640d13b0d501d1ae91b4d0fd954
DESKTOP-OHU416I\Ronald <rruanov1@miumg.edu.gt>
2021-10-30 17:25:55 -0600

    vista pagos Ronald Ruano

---
+++ src/app/componentes/pagos/pagos/pagos.component.ts
@@ -0,0 +1,57 @@
+import { TablaPagosItem } from './../tabla-pagos/tabla-pagos-datasource';
+import { ApiService } from './../../services/api.service';
+import { Router } from '@angular/router';
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-pagos',
+  templateUrl: './pagos.component.html',
+  styleUrls: ['./pagos.component.css']
+})
+export class PagosComponent {
+  addressForm!: FormGroup;
+  /*addressForm = this.fb.group({
+    company: null,
+    firstName: [null, Validators.required],
+    lastName: [null, Validators.required],
+    address: [null, Validators.required],
+    address2: null,
+    city: [null, Validators.required],
+    state: [null, Validators.required],
+    postalCode: [null, Validators.compose([
+      Validators.required, Validators.minLength(5), Validators.maxLength(5)])
+    ],
+    shipping: ['free', Validators.required]
+  });*/
+
+  constructor(private fb: FormBuilder, private Router:Router, private ApiService:ApiService) {
+    this.addressForm = this.fb.group({
+      numero_factura:[''],
+      id_boleto: ['', Validators.required],
+      costo_total: ['', Validators.required],
+      Tarjeta_Credito: ['', Validators.required],
+      Nombre_Propietario: ['', Validators.required],
+      Fecha_Exp: ['', Validators.required],
+      cvv: ['', Validators.required]
+
+    });
+  }
+
+  onSubmit(){
+    console.log(this.addressForm)
+    const user: TablaPagosItem ={
+      numero_factura: this.addressForm.value.numero_factura,
+      id_boleto: this.addressForm.value.id_boleto,
+      costo_total: this.addressForm.value.costo_total,
+      Tarjeta_Credito: this.addressForm.value.Tarjeta_Credito,
+      Nombre_Propietario: this.addressForm.value.Nombre_Propietario,
+      Fecha_Exp: this.addressForm.value.Fecha_Exp,
+      cvv: this.addressForm.value.cvv,
+    }
+    console.log(user)
+    this.ApiService.addpago(user).subscribe();
+    this.Router.navigate(['/pagos'])
+  }
+
+}
---
+++ src/app/componentes/pagos/tabla-pagos/tabla-pagos.component.spec.ts
@@ -0,0 +1,34 @@
+import { waitForAsync, ComponentFixture, TestBed } from '@angular/core/testing';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
+import { MatTableModule } from '@angular/material/table';
+
+import { TablaPagosComponent } from './tabla-pagos.component';
+
+describe('TablaPagosComponent', () => {
+  let component: TablaPagosComponent;
+  let fixture: ComponentFixture<TablaPagosComponent>;
+
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      declarations: [ TablaPagosComponent ],
+      imports: [
+        NoopAnimationsModule,
+        MatPaginatorModule,
+        MatSortModule,
+        MatTableModule,
+      ]
+    }).compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(TablaPagosComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should compile', () => {
+    expect(component).toBeTruthy();
+  });
+});
